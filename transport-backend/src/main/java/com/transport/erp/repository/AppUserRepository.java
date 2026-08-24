24cc7ff1bfa4f7c30405b6d8c550f98a137b321f
Mohanrajmohanms7 <145020971+Mohanrajmohanms7@users.noreply.github.com>
2026-08-24 23:05:55 +0530

    Feature: Added EntityGraph to AppUserRepository to eagerly load roles on getUsers list and resolve lazy load patch value issue

--- transport-backend/src/main/java/com/transport/erp/repository/AppUserRepository.java
+++ transport-backend/src/main/java/com/transport/erp/repository/AppUserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.EntityGraph;
 import java.util.Optional;
 
 @Repository
@@ -13,8 +14,13 @@ public interface AppUserRepository extends JpaRepository<AppUser, Long> {
     boolean existsByUsernameAndIsDeletedFalse(String username);
     Optional<AppUser> findByCodeAndIsDeletedFalse(String code);
     Optional<AppUser> findByCompanyIdAndCodeAndIsDeletedFalse(Long companyId, String code);
+
+    @EntityGraph(attributePaths = {"roles"})
     Page<AppUser> findByCompanyIdAndIsDeletedFalse(Long companyId, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"roles"})
     Page<AppUser> findByCompanyIdAndIsDeletedFalseAndNameContainingIgnoreCaseOrCodeContainingIgnoreCase(Long companyId, String name, String code, Pageable pageable);
+
     Page<AppUser> findByIsDeletedFalse(Pageable pageable);
     Page<AppUser> findByIsDeletedFalseAndUsernameContainingIgnoreCaseOrIsDeletedFalseAndEmailContainingIgnoreCase(String username, String email, Pageable pageable);
 }
